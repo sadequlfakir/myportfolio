4f9c23a0849def3a3c760f6e962e48100cc89e2e
sadequlfakir <sadikul0140@gmail.com>
2024-05-22 16:30:02 +0600

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,228 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  team,
+  bellbird,
+  chai,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'work',
+    title: 'Work',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Front End Web Developer',
+    icon: web,
+  },
+  {
+    title: 'React Native Developer',
+    icon: mobile,
+  },
+  //   title: "Content Creator",
+  //   icon: creator,
+  // },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  // {
+  //   name: 'TypeScript',
+  //   icon: typescript,
+  // },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Three JS',
+    icon: threejs,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'figma',
+    icon: figma,
+  },
+  // {
+  //   name: 'docker',
+  //   icon: docker,
+  // },
+];
+
+const experiences = [
+  {
+    title: 'Front End Developer',
+    company_name: 'Team',
+    icon: team,
+    iconBg: '#383E56',
+    date: 'January 2022 - Continuing',
+    points: [
+      'Developing and maintaining web applications using React.js and other related technologies.',
+      // 'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+      'Implementing responsive design and ensuring cross-browser compatibility.',
+      // 'Participating in code reviews and providing constructive feedback to other developers.',
+    ],
+  },
+  // {
+  //   title: 'Front End Developer',
+  //   company_name: 'Bellbird Technologies',
+  //   icon: bellbird,
+  //   iconBg: '#E6DEDD',
+  //   date: 'May 2024 - Continuing',
+  //   points: [
+  //     'Developing and maintaining web applications using React.js and other related technologies.',
+  //     'Implementing responsive design and ensuring cross-browser compatibility.',
+  //   ],
+  // },
+  // {
+  //   title: 'Web Developer',
+  //   company_name: 'Shopify',
+  //   icon: shopify,
+  //   iconBg: '#383E56',
+  //   date: 'Jan 2022 - Jan 2023',
+  //   points: [
+  //     'Developing and maintaining web applications using React.js and other related technologies.',
+  //     'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+  //     'Implementing responsive design and ensuring cross-browser compatibility.',
+  //     'Participating in code reviews and providing constructive feedback to other developers.',
+  //   ],
+  // },
+  // {
+  //   title: 'Full stack Developer',
+  //   company_name: 'Meta',
+  //   icon: meta,
+  //   iconBg: '#E6DEDD',
+  //   date: 'Jan 2023 - Present',
+  //   points: [
+  //     'Developing and maintaining web applications using React.js and other related technologies.',
+  //     'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
+  //     'Implementing responsive design and ensuring cross-browser compatibility.',
+  //     'Participating in code reviews and providing constructive feedback to other developers.',
+  //   ],
+  // },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      'I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.',
+    name: 'Sara Lee',
+    designation: 'CFO',
+    company: 'Acme Co',
+    image: 'https://randomuser.me/api/portraits/women/4.jpg',
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: 'Chris Brown',
+    designation: 'COO',
+    company: 'DEF Corp',
+    image: 'https://randomuser.me/api/portraits/men/5.jpg',
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: 'Lisa Wang',
+    designation: 'CTO',
+    company: '456 Enterprises',
+    image: 'https://randomuser.me/api/portraits/women/6.jpg',
+  },
+];
+
+const projects = [
+  {
+    name: 'Buy Me A Chai',
+    description:
+      'A place where your fans can buy you a chai. Unleash the power of your fans and get your projects funded.',
+    tags: [
+      {
+        name: 'next.js',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'auth.js',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: chai,
+    source_code_link: 'https://bitbucket.org/sadequlfakir/buymeachai',
+  },
+  {
+    name: 'Snap SASS Ai',
+    description:
+      'A platform where anyone can restore his image, Generative Fill,Object Remove, Object Recolor his photos with ai',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'clerk',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: tripguide,
+    source_code_link: 'https://github.com/sadequlfakir/snap-sass',
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
